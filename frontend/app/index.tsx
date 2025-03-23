cab39aa7a7635070189867a762375a3cf266b9d3
Mateusz Dalke <sculpttechproject@gmail.com>
2025-03-23 23:03:50 +0100

    Initial commit: working expo, nativewind; expressjs

---
+++ frontend/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-green-100">
+      <Text className="font-bold drop-shadow-2xl text-4xl text-gray-800">
+        Management Control
+      </Text>
+    </View>
+  );
+}
