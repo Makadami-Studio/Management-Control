2100259313f746b7cf38b5dc1e8b4c710aa5a806
RexioxD <rexio20051919@gmail.com>
2025-03-26 01:14:36 +0100

    Created the login page and main, login redirect you to main but not yet working to check if you give him the correct data, need to add functionality for loggin properlly

---
+++ frontend/app/(auth)/main/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+
+const Main = () => {
+    return (
+        <View className="items-center justify-center flex-1 gap-6">
+            <Text className='font-bold text-4xl'>Witaj w Mainie :3</Text>
+            <Text className='font-bold text-2xl'>Coś tu bedzie gotowane jescze </Text>
+            <Text className='font-bold text-2xl text-secondary'>STAY TUNED!!!</Text>
+        </View>
+    )
+}
+export default Main
+const styles = StyleSheet.create({})
