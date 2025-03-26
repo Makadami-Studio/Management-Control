1abaa151f6b4466b9bafe8faf02919ac779988e7
Mateusz Dalke <sculpttechproject@gmail.com>
2025-03-26 19:31:32 +0100

    Make text for index, changed main (dashboard) directory

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
   const router = useRouter();
 
   const upslideAnim = useRef(new Animated.Value(-100)).current; //up slide ref
-    const downslideAnim = useRef(new Animated.Value(100)).current; //down slide ref
+  const downslideAnim = useRef(new Animated.Value(100)).current; //down slide ref
   const fadeAnim = useRef(new Animated.Value(0)).current; //fade-in ref
 
   // animacje na wejscie
@@ -31,100 +31,103 @@ export default function Index() {
         duration: 800,
         useNativeDriver: true,
       }),
-
     ]).start();
   }, []);
 
   return (
-      //caly ekran
-      <SafeAreaView className="flex-auto bg-white">
-        {/* Main view */}
+    //caly ekran
+    <SafeAreaView className="flex-auto bg-white">
+      {/* Main view */}
 
-          <View className="items-center justify-start flex-1 mt-10">
-            <Animated.View
-                /* slide w Y            fade-in (opacity) */
-                style={{ transform: [{ translateY: downslideAnim }], opacity: fadeAnim }}
-            >
-                <Text className="text-4xl font-bold text-third drop-shadow-2xl ite">
-                    Welcome To the
-                </Text>
-            </Animated.View>
-          <Animated.View
-              /* slide w Y            fade-in (opacity) */
-              style={{ transform: [{ translateY: upslideAnim }], opacity: fadeAnim }}
-          >
-            {/* Text jako logo ;D */}
-            <Text className="text-4xl font-bold text-secondary drop-shadow-2xl">
-              Management Control
+      <View className="items-center justify-start flex-1 pt-8 mt-10">
+        <Animated.View
+          /* slide w Y            fade-in (opacity) */
+          style={{
+            transform: [{ translateY: downslideAnim }],
+            opacity: fadeAnim,
+          }}
+        >
+          <Text className="text-4xl font-bold text-third drop-shadow-2xl ite">
+            Welcome To the
+          </Text>
+        </Animated.View>
+        <Animated.View
+          /* slide w Y            fade-in (opacity) */
+          style={{
+            transform: [{ translateY: upslideAnim }],
+            opacity: fadeAnim,
+          }}
+        >
+          {/* Text jako logo ;D */}
+          <Text className="text-4xl font-bold text-secondary drop-shadow-2xl">
+            Management Control
+          </Text>
+        </Animated.View>
+      </View>
+      <View className="items-center justify-start flex-1 gap-5 ">
+        <TouchableOpacity
+          className="flex-row items-center justify-center w-80 h-14"
+          onPress={() => {
+            router.push("/(dashboard)/main"); //narazie przerzuca testowo do maina pozdro #do zmiany :3
+          }}
+        >
+          <View className="flex-row items-center justify-center flex-shrink gap-1 py-4 text-base border w-80 border-third rounded-3xl">
+            <Image
+              source={require("..//assets/images/google.png")}
+              className="w-8 h-8 mr-2"
+              resizeMode="contain"
+            />
+            <Text className="text-base whitespace-nowrap">
+              Sign in with Google
             </Text>
-          </Animated.View>
           </View>
-          <View className="items-center justify-start flex-1 gap-5 ">
-              <TouchableOpacity className="w-80 h-14 flex-row items-center justify-center"
-                  onPress={() => {
-                      router.push("/(auth)/main");  //narazie przerzuca testowo do maina pozdro #do zmiany :3
-                  }}
-              >
-                  <View className="w-80 gap-1 py-4 text-base border border-third rounded-3xl justify-center items-center flex-row  flex-shrink">
-                      <Image
-                          source={require("..//assets/images/google.png")}
-                          className="w-8 h-8 mr-2"
-                          resizeMode="contain"
-                      />
-                      <Text className="text-base whitespace-nowrap">
-                          Sign in with Google
-                      </Text>
-                  </View>
-              </TouchableOpacity>
-              <TouchableOpacity className="w-80 h-14 flex-row items-center justify-center "
-                  onPress={() => {
-                      router.push("/(auth)/main");  //narazie przerzuca testowo do maina pozdro #do zmiany :3
-                  }}
-              >
-                  <View className="w-80 gap-1 py-4 text-base border border-third rounded-3xl justify-center items-center flex-row ">
-                      <Image
-                          source={require("..//assets/images/facebook.png")}
-                          className="w-8 h-8 mr-2"
-                          resizeMode="contain"
-                      />
-                      <Text className="text-base whitespace-nowrap">
-                          Sign in with Facebook
-                      </Text>
-                  </View>
-              </TouchableOpacity>
-          {/* View dla buttonow */}
-          <View className=" items-center flex-1  justify-start  ">
-            {/* Login btn */}
-            <TouchableOpacity
-                onPress={() => {
-                  router.push("/(auth)/login");
-                }}
-                className="px-6 py-3 mt-4 bg-buttonOne rounded-xl"
-            >
-              <Text className="text-lg font-semibold text-white">
-                  Login
-              </Text>
-            </TouchableOpacity>
-            {/* Register btn */}
-            <TouchableOpacity
-                onPress={() => {
-                  router.push("/(auth)/register");
-                }}
-                className="px-6 py-3 mt-4 bg-buttonTwo rounded-xl"
-            >
-              <Text className="text-lg font-semibold text-white">
-                  Register
-              </Text>
-            </TouchableOpacity>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="flex-row items-center justify-center w-80 h-14 "
+          onPress={() => {
+            router.push("/(dashboard)/main"); //narazie przerzuca testowo do maina pozdro #do zmiany :3
+          }}
+        >
+          <View className="flex-row items-center justify-center gap-1 py-4 text-base border w-80 border-third rounded-3xl ">
+            <Image
+              source={require("..//assets/images/facebook.png")}
+              className="w-8 h-8 mr-2"
+              resizeMode="contain"
+            />
+            <Text className="text-base whitespace-nowrap">
+              Sign in with Facebook
+            </Text>
           </View>
-          </View>
-        {/* Obrazek jakis spoko ;D */}
-        <Image
-            source={require("..//assets/images/workTogether.png")}
-            className="absolute bottom-0 self-center w-64 h-64"
-            resizeMode="contain"
-        />
-
-      </SafeAreaView>
+        </TouchableOpacity>
+        {/* View dla buttonow */}
+        <Text className="mt-5 font-semibold text-secondary drop-shadow-xl">Email or phone numer:</Text>
+        <View className="flex flex-row items-center justify-start gap-6">
+          {/* Login btn */}
+          <TouchableOpacity
+            onPress={() => {
+              router.push("/(auth)/login");
+            }}
+            className="px-8 py-3 bg-buttonOne rounded-xl"
+          >
+            <Text className="text-lg font-semibold text-white">Login</Text>
+          </TouchableOpacity>
+          {/* Register btn */}
+          <TouchableOpacity
+            onPress={() => {
+              router.push("/(auth)/register");
+            }}
+            className="px-6 py-3 bg-buttonTwo rounded-xl"
+          >
+            <Text className="text-lg font-semibold text-white">Register</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* Obrazek jakis spoko ;D */}
+      <Image
+        source={require("..//assets/images/workTogether.png")}
+        className="absolute bottom-0 self-center w-64 h-64"
+        resizeMode="contain"
+      />
+    </SafeAreaView>
   );
 }
